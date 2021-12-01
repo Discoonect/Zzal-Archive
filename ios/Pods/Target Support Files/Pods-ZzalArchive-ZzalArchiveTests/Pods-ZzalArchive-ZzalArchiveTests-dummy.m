bc800fd30e842edad48c74360008129e5f8c16de
kisung <67423146+Discoonect@users.noreply.github.com>
2021-12-01 22:05:09 +0900

    init
    생성

---
+++ ios/Pods/Target Support Files/Pods-ZzalArchive-ZzalArchiveTests/Pods-ZzalArchive-ZzalArchiveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZzalArchive_ZzalArchiveTests : NSObject
+@end
+@implementation PodsDummy_Pods_ZzalArchive_ZzalArchiveTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ZzalArchive/Pods-ZzalArchive-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZzalArchive : NSObject
+@end
+@implementation PodsDummy_Pods_ZzalArchive
+@end
